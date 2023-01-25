ca853c288a177dca528a722b5f1c86f52796fb47
tcoentoro <tcoentoro@gmail.com>
2023-01-24 23:17:54 -0800

    fixed

--- 23fizzbuzz.py
+++ 23fizzbuzz.py
@@ -8,11 +8,10 @@
 # Note: this is a common interview question
 
 
-for i in range(0, 100, 1) :
-	i += 1
+for i in range(1, 100, 1) :
 	if i%3 != 0 and i%5 != 0 : print(i)
-	if i%3 == 0 : print ('Fizz')
-	if i%5 == 0 : print ('Buzz')
+	if i%3 == 0 and i%5 != 0 : print ('Fizz')
+	if i%3 != 0 and i%5 == 0 : print ('Buzz')
 	if i%3 == 0 and i%5 == 0 : print('FizzBuzz')
 
 """
