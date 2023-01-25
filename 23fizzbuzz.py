7c039b9db91a4454b61a0110ec0f33e4871a32df
tcoentoro <tcoentoro@gmail.com>
2023-01-25 10:25:39 -0800

    otherway

--- 23fizzbuzz.py
+++ 23fizzbuzz.py
@@ -19,7 +19,7 @@ for i in range(1, 101, 1) :
 	if 		i%3 != 0 and i%5 != 0 : print(i)
 	elif 	i%3 == 0 and i%5 == 0 : print('FizzBuzz')
 	elif 	i%3 != 0			  : print('Buzz')
-	else: 							print('Fizz')
+	else:	print('Fizz')
 	
 """
 python3 23fizzbuzz.py
