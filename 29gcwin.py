059a35a2c80630af77bc6138225761cc4fb73653
tcoentoro <tcoentoro@gmail.com>
2023-01-27 10:03:29 -0800

    new

--- 29gcwin.py
+++ 29gcwin.py
@@ -13,7 +13,13 @@
 
 seq = 'ACGACGCAGGAGGAGAGTTTCAGAGATCACGAATACATCCATATTACCCAGAGAGAG'
 w = 11
-
+for i in range(0, len(seq) -w +1):
+	frame = seq[i:i+w]
+	countcg = 0
+	for j in range(len(frame)):
+		if frame[j] == 'C' or frame[j] == 'G': countcg +=1
+		percentage = (countcg/len(frame))
+	print(i, frame, f'{percentage:.4f}')		
 
 """
 python3 26gcwin.py
