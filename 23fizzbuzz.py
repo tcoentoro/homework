1367aa47bd662af445ed5f9c5dfc75e0ff8a06e0
tcoentoro <tcoentoro@gmail.com>
2023-01-26 22:23:21 -0800

    neat2

--- 23fizzbuzz.py
+++ 23fizzbuzz.py
@@ -16,10 +16,10 @@ for i in range(1, 101, 1) : # First try
 """
 
 for i in range(1, 101, 1) :
-	if 		i%3 != 0 and i%5 != 0	: print(i)
-	elif 	i%3 == 0 and i%5 == 0	: print('FizzBuzz')
-	elif	i%3 != 0				: print('Buzz')
-	else							: print('Fizz')
+	if		i%3 != 0 and i%5 != 0: print(i)
+	elif	i%3 == 0 and i%5 == 0: print('FizzBuzz')
+	elif	i%3 != 0			: print('Buzz')
+	else						: print('Fizz')
 	
 """
 python3 23fizzbuzz.py
