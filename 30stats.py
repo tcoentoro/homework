e92fd09af699ca0854af5d6e0c1273c9139e0704
tcoentoro <tcoentoro@gmail.com>
2023-02-08 20:02:45 -0800

    initial

--- 30stats.py
+++ 30stats.py
@@ -7,6 +7,49 @@
 
 # Note: you are not allowed to import any library except sys
 
+import sys
+
+#List sorting
+numbers = []
+
+for num in sys.argv[1:]:
+	numbers.append(float(num))
+
+numbers.sort() 
+
+
+#Calculations: Total + mean
+total = sum(numbers)
+mean = total/len(numbers)
+mid = len(numbers) // 2
+
+
+#Calculations: Median
+median = None
+
+if len(numbers) % 2 == 0:
+	median = (numbers[mid] + numbers[mid+1])/2
+else:
+	median = numbers[mid]
+
+
+#Calculations: Standard Deviation
+sumdistance = 0
+
+for x in numbers:
+	sumdistance += (x - mean) ** 2
+	
+sd = (sumdistance/len(numbers)) ** 0.5
+
+
+#Output
+print(numbers)
+print(f'Count: {len(numbers)}')
+print(f'Minimum: {numbers[0]:.1f}')
+print(f'Maximum: {numbers[-1]:.1f}')
+print(f'Mean: {(mean):.3f}')
+print(f'Std. dev: {sd:.3f}')
+print(f'Median {median:.3f}')
 
 """
 python3 30stats.py 3 1 4 1 5
