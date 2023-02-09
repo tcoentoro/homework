6c4a5fea5d14dfff6575abacb85f24e14348c7a0
tcoentoro <tcoentoro@gmail.com>
2023-02-09 12:58:43 -0800

    initialhw

--- 31entropy.py
+++ 31entropy.py
@@ -10,6 +10,38 @@
 
 # Hint: try breaking your program with erroneous input
 
+#Libraries
+import sys
+import math
+
+
+#List of Values
+vals = []
+
+for val in sys.argv[1:]:
+	try:
+		p = float(val)
+	except:
+		raise ValueError(f'cannot convert "{i}" to a number')
+	vals.append(float(val))
+
+#Total prob = 1
+total_1 = sum(vals)
+
+try:
+	assert(math.isclose(total_1, 1.0))
+except:
+	raise ValueError(f'The sum of your probabilities is not 1')
+
+#Calculations
+entropy = 0
+
+for j in vals:
+	entropy += -(j * math.log2(j))
+
+#Output
+print(f'{entropy:.3f}')
+
 
 """
 python3 31entropy.py 0.1 0.2 0.3 0.4
