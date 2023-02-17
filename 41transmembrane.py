96d0eac2df8fd874134c938599ff1072ec239ecf
tcoentoro <tcoentoro@gmail.com>
2023-02-16 20:57:33 -0800

    incomplete

--- 41transmembrane.py
+++ 41transmembrane.py
@@ -21,6 +21,42 @@
 # Hint: create a function for hydrophobic alpha-helix
 # Hint: use the same function for both signal peptide and transmembrane
 
+import sys
+import mcb185
+import random
+
+sample = ''
+aas = 'ACDEFGHIKLMNPQRSTVWY'
+
+for hehe in range(20):
+	sample += random.choice(aas)
+
+def kd_hydrop(aa):
+	hydrop = 0
+
+	aas = 'ACDEFGHIKLMNPQRSTVWY'
+	aas_hs = [1.8, 2.5, -3.5, -3.5, 2.8, -0.4, -3.2, 4.5, -3.9, 3.8, 1.9, -3.5, -1.6, -3.5, -4.5, -0.8, -0.7, 4.2, -0.9, -1.3]
+	
+	hydrop += aas_hs[aas.find(aa)]
+	return hydrop
+
+def ave_hydrop(seq):
+	total_hydrop = 0
+	
+	for i in range(len(seq)):
+		total_hydrop += kd_hydrop(seq[i])
+	
+	average = total_hydrop/len(seq)
+	return average
+
+
+print(sample, ave_hydrop(sample))
+"""
+for defline, seq in mcb185.read_fasta(sys.argv[1]):
+	words = defline.split()
+	name = words[0]
+	print(name, defline, seq[0:20])
+"""
 
 """
 python3 41transmembrane.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_protein.faa.gz
