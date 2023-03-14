68fa6697cb32afb70b7163cfae7c9ed33d906af3
tcoentoro <tcoentoro@gmail.com>
2023-03-13 20:31:15 -0700

    incomplete

--- 63canonical.py
+++ 63canonical.py
@@ -14,6 +14,44 @@
 # Hint: check your CDS by examining the source protein
 
 
+import argparse
+import re
+import gzip
+import mcb185
+
+parser = argparse.ArgumentParser(description = 'ATG Finder RegEx')
+parser.add_argument('file', type=str, metavar='<path>', help='genomic bank file')
+arg = parser.parse_args()
+
+sequence = ""
+
+#will ignore any pseudogenes
+with gzip.open(arg.file, 'rt') as fp:
+	for line in fp:
+		if line.startswith('ORIGIN'):
+			break
+	for line in fp:
+		f = line.split()
+		sequence += "".join(f[1:])
+
+#print(len(sequence))
+
+with gzip.open(arg.file, 'rt') as fp:
+	for line in fp:
+		for match in re.finditer('\s(CDS)\s', line):
+			
+			coor = re.search('(\d+)\.\.(\d+)', line)
+			beg = int(coor.group(1))
+			end = int(coor.group(2))
+			
+			if 'complement' in line:
+				nyeh = mcb185.reverse(sequence[end-3:end])
+			
+			#print(sequence[beg-1:beg+2])
+			#if 'complement' in line:
+			#print(line, coor.group(1),)
+
+
 """
 python3 63canonical.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_genomic.gbff.gz
 ATG 3883
